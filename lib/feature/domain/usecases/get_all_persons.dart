4e40d54af00204c29bb353085937569e6cf6cb55
lexlibman <lexlibman@gmail.com>
2022-09-24 14:32:01 +0300

    models

--- lib/feature/domain/usecases/get_all_persons.dart
+++ lib/feature/domain/usecases/get_all_persons.dart
@@ -1,15 +1,28 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:rick_and_morty/core/usecases/usecase.dart';
 import 'package:rick_and_morty/feature/domain/repositories/person_repository.dart';
 
 import '../../../core/error/failure.dart';
 import '../entities/person_entity.dart';
 
-class GetAllPersons {
+class GetAllPersons extends UseCase<List<PersonEntity>, PagePersonParams> {
   final PersonRepository personRepository;
 
   GetAllPersons(this.personRepository);
 
-  Future<Either<Failure, List<PersonEntity>>> call(int page) async {
-    return await personRepository.getAllPersons(page);
+  @override
+  Future<Either<Failure, List<PersonEntity>>> call(
+      PagePersonParams params) async {
+    return await personRepository.getAllPersons(params.page);
   }
+}
+
+class PagePersonParams extends Equatable {
+  final int page;
+
+  const PagePersonParams({required this.page});
+
+  @override
+  List<Object> get props => [page];
 }
